51538c0e629f64b52e1cff82b8220c08efad2eb1
studentqa2021 <sarowerhome@gmail.com>
2022-01-12 17:04:53 -0500

    initial commit

---
+++ bdcore-automation/src/main/java/com/practice2/Animal.java
@@ -0,0 +1,11 @@
+package com.practice2;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		Human obj = new Human("Hi");
+		
+		obj.getSalary();
+	}
+
+}
---
+++ bdcore-automation/src/main/java/com/practice2/Human.java
@@ -0,0 +1,22 @@
+package com.practice2;
+
+public class Human {
+	
+	//code= method,variable,obj,data,constructor
+
+	public  String  name;
+	
+	public Human(String  name){
+		this.name=name;
+		
+	}
+	
+	public void getSalary(){
+		
+		System.out.println(name);
+	}
+	
+	
+	
+
+}
